bd13991acded49c6379575a9501403abe45cf367
gavin <audesandrine6@gmail.com>
2021-12-13 14:44:25 +0100

    first commit

---
+++ include.php
@@ -0,0 +1,13 @@
+<?php
+function logger($log){ 
+    if(!file_exists('log.txt')){
+        file_put_contents('log.txt','');
+    }
+    $ip = $_SERVER['REMOTE_ADDR'];
+    $time= date('m/d/Y H:i:s , time()');
+    $content = file_get_contents('log.txt');
+    $content .="$ip\$time\$log\r";
+
+    file_put_contents("log.txt", $content);
+}
+ ?>
---
+++ index.php
@@ -0,0 +1,29 @@
+<?php 
+include'./include.php';
+if($_SERVER['REQUEST_METHOD']==='POST'){
+    $number= $_POST['number'];
+    if($number!= 5){
+        $log=("User put incorrect number ($number");
+        logger('$log');
+        echo "Invalid number";
+    }else{
+        echo "Valid number";
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form method="post">
+    <input type="Text" name="number">
+    <input type="submit">
+</form>
+    
+</body>
+</html>
